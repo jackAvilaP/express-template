323ea96ef82527162e812c68943e6a443259eadc
Jackson avila paez <106121080+jackAvilaP@users.noreply.github.com>
2023-03-30 07:48:08 -0500

    Update database.js
    modificate dialect

--- utils/database.js
+++ utils/database.js
@@ -5,7 +5,7 @@ dotenv.config({ path: './config.env' });
 
 // Create a connection to database
 const db = new Sequelize({
-  dialect: 'mysql',
+  dialect: process.env.DB_TYPE, // type data base (mysql, mongo, pg, ...)
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
